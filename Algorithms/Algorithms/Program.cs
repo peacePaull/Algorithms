5c131775dac561a95859af76f5b64c95a103c648
Peace Adegunju <peacemastre@gmail.com>
2023-06-21 17:00:37 +0100

    solved FIrst Bad Version

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -37,8 +37,49 @@
 
         #endregion
 
-    }
-    public class Program
+        #region First Bad Version
+
+        //You are a product manager and currently leading a team to develop a new product.Unfortunately,
+        //the latest version of your product fails the quality check.Since each version is developed based on the previous version,
+        //all the versions after a bad version are also bad.
+        //Suppose you have n versions[1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+        //You are given an API bool isBadVersion(version) which returns whether version is bad.Implement a function to find the first bad version.
+        //You should minimize the number of calls to the API.
+
+
+        public int FirstBadVersion(int n)
+        {
+            int left = 1;
+            int right = n;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (IsBadVersion(mid))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+
+        private bool IsBadVersion(int mid)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+}
+public class Program
     {
         public static void Main()
         {
@@ -51,6 +92,17 @@
             int result = solution.Search(nums, target);
 
             Console.WriteLine("Index of target: " + result);
+
+            #endregion
+
+            #region First Bad Version
+
+            int n = 5;
+            int bad = 4;
+
+            int firstBadVersion = solution.FirstBadVersion(n);
+
+            Console.WriteLine("First Bad Version: " + firstBadVersion);
 
             #endregion
         }
