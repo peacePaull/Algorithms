08b42e135aa85e59804c7dcfedca3c9696b4d4cc
Peace Adegunju <peacemastre@gmail.com>
2023-06-22 06:21:29 +0100

    solved SortedSquares

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -120,6 +120,33 @@ namespace Algorithms
 
         #endregion
 
+        #region SortedSquares
+
+        //Given an integer array nums sorted in non-decreasing order,
+        //return an array of the squares of each number sorted in non-decreasing order.
+
+        //Example 1:
+
+        //Input: nums = [-4, -1, 0, 3, 10]
+        //Output: [0,1,9,16,100]
+        //Explanation: After squaring, the array becomes[16, 1, 0, 9, 100].
+        //After sorting, it becomes [0,1,9,16,100].
+
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] squares = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                squares[i] = nums[i] * nums[i];
+            }
+
+            Array.Sort(squares);
+
+            return squares;
+        }
+
+        #endregion
 
     }
     public class Program
@@ -161,6 +188,12 @@ namespace Algorithms
             Console.WriteLine("Index: " + index);
 
             #endregion
+
+            int[] numsArr = { -4, -1, 0, 3, 10 };
+
+            int[] sortedResult = solution.SortedSquares(numsArr);
+
+            Console.WriteLine("Output: [" + string.Join(", ", sortedResult) + "]");
         }
     }
 }
